e4bd84ca473c8aaa6604acdd8d37d601a951da8d
Frederic Bonifas <frederic.bonifas.ext@makina-corpus.com>
2013-06-28 14:16:05 +0200

    frontend

--- preprocessing/routinggraph/config.py
+++ preprocessing/routinggraph/config.py
@@ -4,20 +4,23 @@
 # Location of a local extract if any, for use with -e option.  Please also define the correct bounding box above
 osm_extract="/home/makina/Téléchargements/monaco-latest.osm"
 
-#bbox="1.35,43.53,1.52,43.67"
-#lon_inf="1.18"
-#lon_sup="1.64"
-#lat_inf="43.50"
-#lat_sup="43.77"
-lon_inf="1.39"
-lon_sup="1.49"
-lat_inf="43.55"
-lat_sup="43.65"
+# Grand Toulouse
+lon_inf="1.18"
+lon_sup="1.64"
+lat_inf="43.50"
+lat_sup="43.77"
+
+# Capitole et Place Wilson
+#lon_inf="1.44"
+#lon_sup="1.45"
+#lat_inf="43.60"
+#lat_sup="43.61"
+
+# DB parameters
 db_name="routing"
 db_user="routing"
 db_pass="KadufZyn8Dr"
 
 # List of walkable areas
-pedestrianAreasHighway=["pedestrian"]
-pedestrianAreasHighway=["footway"]
+pedestrianAreasHighway=["pedestrian","footway"]
 #pedestrianAreasAmenity=["parking"]
